34a577a19e9410b36de288dfb0caeabce720becb
nediyadath@gmail.com <@Power123>
2022-07-08 15:39:49 +0530

    first commit

---
+++ Billing.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revision
+{
+
+//Partially abstract
+public abstract class MyAbstractClass
+    {
+        public void M2() { }
+        void m1() {
+            Console.Write("Implemented");
+        }
+
+    }
+
+    public class MyClass: MyAbstractClass
+    {
+    }
+
+    //Completely abstract
+    public interface IBilling
+    {
+        void Estimate(float price);
+        void Discount(string custType);
+        void Receipt(float totalPrice);
+
+    }
+    public partial class Billing : IBilling
+    {
+        public void Discount(string custType)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    public partial class Billing: IBilling
+    {
+        public void Estimate(float price)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    public partial class Billing
+    {
+        public void Receipt(float totalPrice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Revision.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revision
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            MyAbstractClass obj = new MyAbstractClass();
+            obj.M2();
+
+        }
+
+    }
+}
